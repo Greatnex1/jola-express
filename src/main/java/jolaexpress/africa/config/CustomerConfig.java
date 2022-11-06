08db9172c5f49935bf76e8b2e8886f428a22c997
Nouah <noahgreatakoni@gmail.com>
2022-11-06 23:24:56 +0100

    api

---
+++ src/main/java/jolaexpress/africa/config/CustomerConfig.java
@@ -0,0 +1,14 @@
+package jolaexpress.africa.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/*
+@Configuration
+public class CustomerConfig {
+
+    @Bean
+    CommandLineRunner
+}
+*/
