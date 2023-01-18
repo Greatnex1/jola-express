e0e6a5c61ebcec1f8761e13cc64083feb4e0a802
Nouah <noahgreatakoni@gmail.com>
2023-01-18 01:50:45 +0100

    customer

---
+++ src/main/java/jolaexpress/africa/exception/CustomerAlreadyExistException.java
@@ -0,0 +1,7 @@
+package jolaexpress.africa.exception;
+
+public class CustomerAlreadyExistException extends Exception {
+    public CustomerAlreadyExistException(String message) {
+        super(message);
+    }
+}
