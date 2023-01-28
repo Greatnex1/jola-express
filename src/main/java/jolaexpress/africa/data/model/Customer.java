be3ec4ef9a359c8d2ad4cbc3d800fbecf89503c4
Nouah <noahgreatakoni@gmail.com>
2023-01-28 16:06:40 +0100

    customer

--- src/main/java/jolaexpress/africa/data/model/Customer.java
+++ src/main/java/jolaexpress/africa/data/model/Customer.java
@@ -20,6 +20,7 @@ public class Customer extends  JolaExpressUser{
     private String email;
     private String phoneNumber;
     private String password;
+    private String resetPassword;
 
     @ManyToOne(cascade = CascadeType.ALL)
     private Address address;
