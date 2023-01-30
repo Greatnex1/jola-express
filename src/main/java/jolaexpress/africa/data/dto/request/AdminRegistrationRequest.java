dbad3df3b84bce66ff0fdaf802dd9ed782008b89
Nouah <noahgreatakoni@gmail.com>
2023-01-30 16:27:06 +0100

    admin

--- src/main/java/jolaexpress/africa/data/dto/request/AdminRegistrationRequest.java
+++ src/main/java/jolaexpress/africa/data/dto/request/AdminRegistrationRequest.java
@@ -10,6 +10,8 @@ import lombok.Setter;
 public class AdminRegistrationRequest {
     private String firstName;
     private String lastName;
+    private String phoneNumber;
     private String email;
     private String password;
+
 }
