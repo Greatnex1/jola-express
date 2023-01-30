086b3219c03a7c4c198add50f78574dc88a02cb1
Nouah <noahgreatakoni@gmail.com>
2023-01-30 11:20:02 +0100

    mail

--- src/main/java/jolaexpress/africa/service/notification/MailNotificationService.java
+++ src/main/java/jolaexpress/africa/service/notification/MailNotificationService.java
@@ -1,7 +1,10 @@
 package jolaexpress.africa.service.notification;
 
 import jolaexpress.africa.data.dto.request.MailNotificationRequest;
+import org.springframework.mail.SimpleMailMessage;
 
 public interface MailNotificationService {
     String sendEmail(MailNotificationRequest mailDetails);
+
+//    void sendMail(SimpleMailMessage email);
 }
